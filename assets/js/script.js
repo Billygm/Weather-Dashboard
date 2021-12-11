e868d70153732afd49379b34629bc5c9922f485d
Billy Mott <billygm@uw.edu>
2021-12-11 09:06:55 -0800

    adding One Call API, havent passed in data yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,34 +3,48 @@ var cityNameEl = document.querySelector("#cityName");
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
-  
-    var cityName = cityNameEl.value.trim();
-  
-    if (cityName) {
-      geoData(cityName);
-  
-      cityNameEl.value = "";
-    } else {
-      alert("Please enter a valid location");
-    }
-  };
 
-var geoData = function(city) {
+    var cityName = cityNameEl.value.trim();
+
+    if (cityName) {
+        geoData(cityName);
+
+        cityNameEl.value = "";
+    } else {
+        alert("Please enter a valid location");
+    }
+};
+
+var geoData = function (city) {
 
     var geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=9b0306363b5cc9091aabbebcc259c820`;
 
     fetch(geoUrl)
-        .then(function(response) {
+        .then(function (response) {
             if (response.ok) {
                 console.log(response);
-                response.json().then(function(data) {
+                response.json().then(function (data) {
                     console.log(data);
+                    oneCall();
                 });
             } else {
                 alert("Error: " + response.statusText);
             }
-            // return response.json();
         });
+}
+
+var oneCall = function (lat, lon) {
+    var oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=imperial&lang=en&appid=9b0306363b5cc9091aabbebcc259c820
+    `;
+
+    fetch(oneCallUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            // render data to page here
+        })
 }
 
 cityInputEl.addEventListener("submit", formSubmitHandler);
